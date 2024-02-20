7dc3de2d3f94ae2a8500da1fa877552341a1856c
Grace-ngigi <gracengigi859@gmail.com>
2024-02-20 06:03:25 +0300

    login &register using jwt auth

--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 ''' user routes '''
 from flask import jsonify, abort, request
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_bcrypt import generate_password_hash, check_password_hash
 from api.v1.views import app_views
 from models.engine.mysqldb_config import MysqlConfig
 from models.user import User
+
+
 
 db = MysqlConfig()
 db.reload()
@@ -21,21 +25,73 @@ def register() -> str:
     if 'password' not in data:
         abort(400, description="Missing password")
 
-    # theres something missing here 
-    # make sure to come back and correct it
     user = db.find_one(User, User.email)
     if user:
         return jsonify(409, description='Email already exists')
+    
+    hashed_password = generate_password_hash(data['password']).decode('utf-8')
     user = User(email=data['email'],
                 phone=data['phone'],
-                password=data['password'])
+                password=hashed_password)
     db.new(user)
     db.save()
-    return jsonify(201, description='User Created')
 
-    
-@app_views.route("/users", methods=['GET'], strict_slashes=False)    
+    token = create_access_token(identity=user.email)
+
+    return jsonify({
+        'access_token': token,
+        'user': {
+            'email': user.email,
+            'phone': user.phone
+        }
+    }), 201
+
+
+@app_views.route("/login", methods=['POST'], strict_slashes=False)
+def login() -> str:
+    ''' login user '''
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    data = request.get_json() 
+    if 'email' not in data:
+        abort(400, description='Missing Email')
+    if 'password' not in data:
+        abort(400, description="Missing password")
+
+    user = db.find_one(User, User.email == data['email'])
+    if not user:
+        abort(401, description="Invalid email or password")
+
+    if not check_password_hash(user.password, data['password']):
+        abort(401, description="Invalid email or password")
+
+    user_data = {
+    'email': user.email,
+    'userid': user.userid,
+    'phone_number': user.phone_number
+    }
+
+    token = create_access_token(identity=user_data)
+
+    return jsonify({
+        'access_token': token,
+        'user': {
+            'email': user.email,
+            'phone': user.phone
+        }
+    }), 200
+
+@app_views.route("/users", methods=['GET'], strict_slashes=False)
+@jwt_required()
 def get_users():
+    current_user = get_jwt_identity()
+    print(current_user)
+    if not current_user:
+        return jsonify(message="Unauthorized"), 401
+    
+    if current_user.get('role') != 'ADMIN':
+        return jsonify(message="Admin privileges required"), 403
+
     users = db.all(User).values()
     users_list = [user.__custom_dict__() for user in users]
     return jsonify(users_list)
--- models/user.py
+++ models/user.py
@@ -3,7 +3,6 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship 
 from .base_model import BaseModel
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(BaseModel):
@@ -16,11 +15,4 @@ class User(BaseModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def set_password(self, password):
-        ''' hash password''' 
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password):
-        """ check if password is correct"""
-        return check_password_hash(self.password, password)  
+        
