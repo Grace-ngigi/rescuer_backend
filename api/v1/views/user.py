1cb6fa92b4bd37fc33d9b90bf4c7990f27df5760
Grace Wamaitha Ngigi <GRACE.NGIGI@equitybank.co.ke>
2024-02-20 16:53:08 +0300

    auth and user complete except serializing dates

--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -6,13 +6,14 @@ from flask_bcrypt import generate_password_hash, check_password_hash
 from api.v1.views import app_views
 from models.engine.mysqldb_config import MysqlConfig
 from models.user import User
+from datetime import datetime
 
 
 
 db = MysqlConfig()
 db.reload()
 
-@app_views.route('/register/', methods=['POST'], strict_slashes=False)
+@app_views.route('/register', methods=['POST'], strict_slashes=False)
 def register() -> str:
     ''' Register new User'''
     if not request.get_json():
@@ -33,16 +34,28 @@ def register() -> str:
     user = User(email=data['email'],
                 phone=data['phone'],
                 password=hashed_password)
+    print(datetime.utcnow())
+    user.created_at = datetime.utcnow()
+    user.created_by = user.id
     db.new(user)
     db.save()
 
-    token = create_access_token(identity=user.email)
+    user_data = {
+    'email': user.email,
+    'id': user.id,
+    'phone': user.phone,
+    'role': user.role
+    }
+
+    token = create_access_token(identity=user_data)
 
     return jsonify({
         'access_token': token,
         'user': {
             'email': user.email,
-            'phone': user.phone
+            'phone': user.phone,
+            'created_at': user.created_at,
+            'created_by': user.created_by
         }
     }), 201
 
@@ -67,8 +80,9 @@ def login() -> str:
 
     user_data = {
     'email': user.email,
-    'userid': user.userid,
-    'phone_number': user.phone_number
+    'id': user.id,
+    'phone': user.phone,
+    'role': user.role
     }
 
     token = create_access_token(identity=user_data)
@@ -85,30 +99,68 @@ def login() -> str:
 @jwt_required()
 def get_users():
     current_user = get_jwt_identity()
-    print(current_user)
     if not current_user:
-        return jsonify(message="Unauthorized"), 401
+        abort(401, description="Unauthorized")
     
     if current_user.get('role') != 'ADMIN':
-        return jsonify(message="Admin privileges required"), 403
+        abort(403, description="Admin privileges required")
 
     users = db.all(User).values()
     users_list = [user.__custom_dict__() for user in users]
     return jsonify(users_list)
 
-@app_views.route("/users/<string:user_id>", methods=['PUT'], strict_slashes=False)
-def update_user(user_id):
-    user = db.find_one(User, User.id == user_id)
+@app_views.route("/profile", methods=['GET'], strict_slashes=False)
+@jwt_required()
+def retrieve_profile():
+    ''' get profile '''
+    current_user = get_jwt_identity()
+    if not current_user:
+        abort(401, description="Unauthorized")
+
+    user = db.find_one(User, User.id == current_user['id'])
+    if not user:
+        abort(404, description="User Not Found")
+
+    print("Retrieved User:", user.__dict__)
+    return jsonify(user.__custom_dict__()), 200
+
+@app_views.route("/profile", methods=['PUT'], strict_slashes=False)
+@jwt_required()
+def update_user():
+    current_user = get_jwt_identity()
+    if not current_user:
+        abort(401, description="Unauthorized")
+    
+    user = db.find_one(User, User.id == current_user['id'])
     if not user:
         abort(400, description = "User not Found")
     if not request.get_json():
         abort(400, description= "Not a JSON")
-    print(user)
     data = request.get_json()
 
     # get to know more about getattr and setattr
     for key, value in data.items():
-        if key != "id" and hasattr(user, key):
+        if key not in ["id", "created_at", "updated_at", "updated_by"] and hasattr(user, key):
             setattr(user, key, value)
+    user.updated_at = datetime.utcnow()
+    user.updated_by = current_user['id']
+
     db.save()
     return jsonify(user.__custom_dict__()), 200
+
+# @app_views.route("/profile", methods=['DELETE'], strict_slashes=False)
+# @jwt_required()
+# def delete_profile():
+#     ''' deleting user profile '''
+#     current_user = get_jwt_identity()
+
+#     if not current_user:
+#         abort(401, description="Unauthorized")
+
+#     user = db.find_one(User, current_user.id)
+#     if not user:
+#         abort(404, description="User Not Found")
+
+    # write code to delete the user
+    # You know, updating the isdeleted flag
+
